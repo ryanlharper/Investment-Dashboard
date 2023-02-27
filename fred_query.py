16765bb738158543966feb3b96116039f69a2b45
Ryan Harper <124845397+ryanlharper@users.noreply.github.com>
2023-02-27 12:58:18 -0500

    Update fred_query.py

--- fred_query.py
+++ fred_query.py
@@ -14,22 +14,22 @@ when = -1
 
 # Dictionary of indicators with their names
 indicators = {'GDP': 'Gross Domestic Product', 
-                   'CPALTT01USM657N': 'Consumer Price Index', 
-                   'UMCSENT': 'University of Michigan Consumer Sentiment Index', 
-                   'PI': 'Personal Income', 
-                   'PCE': 'Personal Consumption Expenditures', 
-                   'ICSA': 'Initial Claims', 
-                   'RSXFS': 'Advance Retail Sales', 
-                   'DFF': 'Federal Funds Effective Rate', 
-                   'EXHOSLUSM495S': 'Existing Home Sales', 
-                   'PPIACO': 'Producer Price Index by Commodity: All Commodities', 
-                   'UNRATE': 'Unemployment Rate', 
-                   'GDPC1': 'Real Gross Domestic Product', 
-                   'CBBTCUSD': 'Coinbase Bitcoin', 
-                   'MICH': 'Inflation Expectation', 
-                   'STICKCPIM157SFRBATL': 'Sticky Price Consumer Price Index', 
-                   'M2SL': 'M2 Money Stock', 
-                   'M2V': 'Velocity of M2'}
+            'CPALTT01USM657N': 'Consumer Price Index', 
+            'UMCSENT': 'University of Michigan Consumer Sentiment Index', 
+            'PI': 'Personal Income', 
+            'PCE': 'Personal Consumption Expenditures', 
+            'ICSA': 'Initial Claims', 
+            'RSXFS': 'Advance Retail Sales', 
+            'DFF': 'Federal Funds Effective Rate', 
+            'EXHOSLUSM495S': 'Existing Home Sales', 
+            'PPIACO': 'Producer Price Index by Commodity: All Commodities', 
+            'UNRATE': 'Unemployment Rate', 
+            'GDPC1': 'Real Gross Domestic Product', 
+            'CBBTCUSD': 'Coinbase Bitcoin', 
+            'MICH': 'Inflation Expectation', 
+            'STICKCPIM157SFRBATL': 'Sticky Price Consumer Price Index', 
+            'M2SL': 'M2 Money Stock', 
+            'M2V': 'Velocity of M2'}
 
 # Get the data for all indicators in the list
 data = {}
