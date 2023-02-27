e7f53b9458b887b1bedc6ed98e746276565b13c3
Ryan Harper <124845397+ryanlharper@users.noreply.github.com>
2023-02-27 12:56:56 -0500

    Update fred_query.py

--- fred_query.py
+++ fred_query.py
@@ -12,19 +12,33 @@ start_date = end_date - datetime.timedelta(days=365 * 1)
 # Set number of days back to retrieve data
 when = -1
 
-# List of indicators
-# GDP, CPI, UM C. Sentiment, Personal Income,  Personal Consumption Expenditures, Initial Claims, 
-# Advance Retail Sales, Federal Funds Effective Rate, Existing Home Sales, Producer Price Index by Commodity: All Commodities 
-# Unemployment Rate, Real GDP, Coinbase Bitcoin
-indicators = ['GDP', 'CPALTT01USM657N', 'UMCSENT', 'PI', 'PCE', 'ICSA', 'RSXFS', 'DFF', 'EXHOSLUSM495S', 'PPIACO', 'UNRATE', 'GDPC1', 'CBBTCUSD']
+# Dictionary of indicators with their names
+indicators = {'GDP': 'Gross Domestic Product', 
+                   'CPALTT01USM657N': 'Consumer Price Index', 
+                   'UMCSENT': 'University of Michigan Consumer Sentiment Index', 
+                   'PI': 'Personal Income', 
+                   'PCE': 'Personal Consumption Expenditures', 
+                   'ICSA': 'Initial Claims', 
+                   'RSXFS': 'Advance Retail Sales', 
+                   'DFF': 'Federal Funds Effective Rate', 
+                   'EXHOSLUSM495S': 'Existing Home Sales', 
+                   'PPIACO': 'Producer Price Index by Commodity: All Commodities', 
+                   'UNRATE': 'Unemployment Rate', 
+                   'GDPC1': 'Real Gross Domestic Product', 
+                   'CBBTCUSD': 'Coinbase Bitcoin', 
+                   'MICH': 'Inflation Expectation', 
+                   'STICKCPIM157SFRBATL': 'Sticky Price Consumer Price Index', 
+                   'M2SL': 'M2 Money Stock', 
+                   'M2V': 'Velocity of M2'}
 
 # Get the data for all indicators in the list
 data = {}
-for indicator in indicators:
+for indicator in indicators.keys():
     data[indicator] = web.DataReader(indicator, 'fred', start_date, end_date, api_key='<your_API_key>')
 
-# Print the last value for each indicator
-for indicator in indicators:
-    print(indicator + ':', data[indicator].iloc[-1][indicator])
+# Print the value for each indicator
+for indicator in indicators.keys():
+    print(indicators[indicator] + ':', data[indicator].iloc[when][indicator])
+
 
 
