6da6053aef317dfacc113402ed503e8a7ecd25bc
Ryan Harper <124845397+ryanlharper@users.noreply.github.com>
2023-02-27 12:32:00 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+import psycopg2
+
+API_KEY ="ca7286427f2a13ee7461c1bd799b3a45"
+
+connection = psycopg2.connect(
+    dbname="investment_dashboard",
+    user="postgres",
+    password="7TQs5%a!SNxvM1By!4El",
+    host="localhost",
+    port="5434"
+)
+
