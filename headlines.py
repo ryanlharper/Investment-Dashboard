ba7505fae4dc89335dcbfc3a7d87ce56e66a09e4
Ryan Harper <124845397+ryanlharper@users.noreply.github.com>
2023-02-28 11:33:43 -0500

    Add files via upload

---
+++ headlines.py
@@ -0,0 +1,35 @@
+import config
+import requests
+from datetime import datetime, timedelta
+
+# Connect to database and query symbols
+conn = config.connection
+cur = conn.cursor()
+cur.execute("""
+    SELECT ws.symbol
+    FROM watchlist_symbols ws
+    WHERE etf = 'No'
+    ORDER BY ws.symbol
+""")
+symbols = [r[0] for r in cur.fetchall()]
+
+# Query Alpha Vantage for headlines
+for symbol in symbols:
+    response = requests.get(f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={symbol}&apikey={config.ALPHA_KEY}")
+    data = response.json()
+    if 'feed' not in data:
+        continue  # skip to the next symbol if feed is empty
+    articles = data['feed']
+    print(f"Recent headlines for {symbol}:")
+    for article in articles:
+        # check if article is within the last 24 hours
+        article_time = datetime.strptime(article['time_published'], '%Y%m%dT%H%M%S')
+        if datetime.now() - article_time > timedelta(hours=24):
+            continue  # skip article if it's older than 24 hours
+        print(article['title'])
+    print("\n")
+
+cur.close()
+conn.close()
+
+
