6928574c77881dad8111a775b16cc6a96b524528
Ryan Harper <124845397+ryanlharper@users.noreply.github.com>
2023-02-28 15:49:27 -0500

    Update headlines.py
    delay added for API limits and readability

--- headlines.py
+++ headlines.py
@@ -1,5 +1,5 @@
 import config
-import requests
+import requests, time
 from datetime import datetime, timedelta
 
 # Connect to database and query symbols
@@ -8,7 +8,7 @@ cur = conn.cursor()
 cur.execute("""
     SELECT ws.symbol
     FROM watchlist_symbols ws
-    WHERE etf = 'No'
+    WHERE ws.etf = 'No' 
     ORDER BY ws.symbol
 """)
 symbols = [r[0] for r in cur.fetchall()]
@@ -26,10 +26,12 @@ for symbol in symbols:
         article_time = datetime.strptime(article['time_published'], '%Y%m%dT%H%M%S')
         if datetime.now() - article_time > timedelta(hours=24):
             continue  # skip article if it's older than 24 hours
+        if 'title' not in article:
+            continue  # skip article if it doesn't have a title
         print(article['title'])
     print("\n")
+    time.sleep(12)  # add a delay of 12 seconds between API requests to stay within the rate limits
+
 
 cur.close()
 conn.close()
-
-
