1de706b156b1eb6ad8be1215f864c84d893010eb
Ryan Harper <124845397+ryanlharper@users.noreply.github.com>
2023-03-01 17:46:06 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import psycopg2
 
-API_KEY ="ca7286427f2a13ee7461c1bd799b3a45"
+API_KEY ="ca7286427f2a13ee7461c1bd799b3a45"  #FRED
 ALPHA_KEY = "V3VL5V9O18Y57H8X" #ALPHA VANTAGE
-NEWS_API_KEY = "bf1039159e414b7b865341206cd8e984" # News API
+NEWS_API_KEY = "bf1039159e414b7b865341206cd8e984" #News API
 
 connection = psycopg2.connect(
     dbname="investment_dashboard",
