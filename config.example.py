75289f58b0990715a47fa4918bb81b13038afb95
Ryan Harper <ryanlharper@gmail.com>
2023-03-01 18:24:37 -0500

    Exclude config.py from Git tracking

---
+++ config.example.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+API_KEY =""  #Source
+ALPHA_KEY = "" #Source
+NEWS_API_KEY = "" #SOurce
+
+connection = psycopg2.connect(
+    dbname="",
+    user="",
+    password="",
+    host="",
+    port=""
+)
