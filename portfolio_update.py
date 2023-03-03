e31db92ee0aadef3d995b39f613e7d40ddfe7200
Ryan Harper <ryanlharper@gmail.com>
2023-03-03 09:49:14 -0500

    add Day(%) column to output

--- portfolio_update.py
+++ portfolio_update.py
@@ -14,7 +14,7 @@ rows = cur.fetchall()
 while True:
     # print headers
     print("\n")
-    print(f"{'Symbol':<8}{'Shares':<8}{'Cost':<12}{'Price':<8}{'% Return':<12}{'$ Return':<12}{'Market Value':<12}")
+    print(f"{'Symbol':<8}{'Shares':<8}{'Cost':<12}{'Price':<8}{'Day (%)' :<8}{'Total (%)':<12}{'Return ($)':<12}{'Market Value ($)':<12}")
     try:
     # query & calculate
         for row in rows:
@@ -22,25 +22,28 @@ while True:
             
             # get the real-time price
             data = yf.Ticker(symbol).history(period='2d')
-            price = data['Close'][-1]
+            latest_price = data['Close'][-1]
+            previous_close = data['Close'][-2]
             
             # calc % return , $ return & mkt_val
             cost, number_shares = float(cost), float(number_shares)
-            r = (price - cost) / cost
-            dollar_return = r * number_shares * price
-            market_value = number_shares * price
+            r = (latest_price - cost) / cost
+            day_gain = (latest_price - previous_close) / previous_close
+            dollar_return = r * number_shares * latest_price
+            market_value = number_shares * latest_price
             
             # update the data in the table
             cur.execute("""
                 UPDATE positions
                 SET price = %s, return = %s, dollar_return = %s, market_value = %s
                 WHERE symbol = %s
-            """, (price, r, dollar_return, market_value, symbol))
+            """, (latest_price, r, dollar_return, market_value, symbol))
             conn.commit()
 
             # print data
-            print(f"{symbol:<8}{int(number_shares):<8}{cost:>8.2f}{price:>8.2f}{r*100:>12.2f}%{dollar_return:>12.2f}{market_value:>12.2f} ")
+            print(f"{symbol:<8}{int(number_shares):<8}{cost:>8.2f}{latest_price:>8.2f}{day_gain*100:>8.2f}%{r*100:>12.2f}%{dollar_return:>12.2f}${market_value:>12.2f} ")
     
+        
         time.sleep(60)
     except KeyboardInterrupt:
         break
