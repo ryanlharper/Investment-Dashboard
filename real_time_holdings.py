28ab779d4224a994cbd15d46e0f7fa45694ffbe0
Ryan Harper <ryanlharper@gmail.com>
2023-03-03 14:51:43 -0500

    changing file name

---
+++ real_time_holdings.py
@@ -0,0 +1,53 @@
+import config, time
+import yfinance as yf
+
+# get positions from table
+conn = config.connection
+cur = conn.cursor()
+cur.execute("""
+    SELECT p.symbol, p.number_shares, p.cost, p.price, p.return, p.dollar_return, p.market_value
+    FROM positions p
+    ORDER BY p.symbol
+""")
+rows = cur.fetchall()
+
+while True:
+    # print headers
+    print("\n")
+    print(f"{'Symbol':<8}{'Shares':<8}{'Cost':<12}{'Price':<8}{'Day (%)' :<8}{'Total (%)':<12}{'Return ($)':<12}{'Market Value ($)':<12}")
+    try:
+    # query & calculate
+        for row in rows:
+            symbol, number_shares, cost, _, _, _, _ = row
+            
+            # get the real-time price
+            data = yf.Ticker(symbol).history(period='2d')
+            latest_price = data['Close'][-1]
+            previous_close = data['Close'][-2]
+            
+            # calc % return , $ return & mkt_val
+            cost, number_shares = float(cost), float(number_shares)
+            r = (latest_price - cost) / cost
+            day_gain = (latest_price - previous_close) / previous_close
+            dollar_return = r * number_shares * latest_price
+            market_value = number_shares * latest_price
+            
+            # update the data in the table
+            cur.execute("""
+                UPDATE positions
+                SET price = %s, return = %s, dollar_return = %s, market_value = %s
+                WHERE symbol = %s
+            """, (latest_price, r, dollar_return, market_value, symbol))
+            conn.commit()
+
+            # print data
+            print(f"{symbol:<8}{int(number_shares):<8}{cost:>8.2f}{latest_price:>8.2f}{day_gain*100:>8.2f}%{r*100:>12.2f}%{dollar_return:>12.2f}${market_value:>12.2f} ")
+    
+        
+        time.sleep(60)
+    except KeyboardInterrupt:
+        break
+
+# close the cursor and connection
+cur.close()
+conn.close()
