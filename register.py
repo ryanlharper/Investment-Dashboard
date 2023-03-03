4ff6ec81697787e2a1d281fc0a4e9eba46a49b59
Ryan Harper <ryanlharper@gmail.com>
2023-03-03 17:46:26 -0500

    fix and error handle

--- register.py
+++ register.py
@@ -1,4 +1,5 @@
 import config, login
+import re, email.utils
 
 conn = config.connection
 cur = conn.cursor()
@@ -15,39 +16,43 @@ while True:
     elif len(username) <=6:
         print("username must be greater than six characters.")
         continue
-    elif len(username) > 6:
-        cur.execute("SELECT username FROM users")
-        users_list = cur.fetchall()
-        if username in [row[0] for row in users_list]:
+    else:
+        cur.execute("SELECT username FROM users WHERE username = %s", (username,))
+        result = cur.fetchone()
+        if result is not None:
             print("username", username, "already registered.")
             continue
-    else:
-        break
+        else:
+            break
 # enter and check email
 while True:
-    email = input('Enter your email or enter q to quit: ') # Cannot be NULL
-    if email == "q":
+    email_str = input('Enter your email or enter q to quit: ')
+    if email_str == "q":
         exit()    
-    elif email == "":
+    elif email_str == "":
         print("An email address must be entered to register.")
         continue
-    elif email != "":
-        cur.execute("SELECT email FROM users")
-        email_list = cur.fetchall()
-        if email in [row[0] for row in email_list]:
-            print("Email adress", email, "is already registered.")
-            continue
-    elif "@" not in email:
-        print("Please enter a valid email address.") 
-        continue
     else:
-        break
+        try:
+            parsed_email = email.utils.parseaddr(email_str)
+            if not re.match(r"[^@]+@[^@]+\.[^@]+", parsed_email[1]):
+                raise ValueError
+            cur.execute("SELECT email FROM users")
+            email_list = cur.fetchall()
+            if parsed_email[1] in [row[0] for row in email_list]:
+                print("Email address", parsed_email[1], "is already registered.")
+                continue
+            else:
+                break
+        except ValueError:
+            print("Please enter a valid email address.")
+            continue
 # enter and check password
 while True:
     password = input('Enter your password or enter q to quit: ') # Cannot be NULL
     if password == "q":
         exit()
-    elif password <=7:
+    elif len(password) <=7:
         print("Password must be eight characters or greater.")
         continue
     else:
@@ -57,7 +62,7 @@ while True:
 hashed_password, salt = login.scramble(password)
 
 # Insert the user into the database with the hashed and salted password
-cur.execute("INSERT INTO users (first_name, last_name, username, email, password, salt) VALUES (%s, %s, %s, %s, %s, %s)", (first_name, last_name, username, email, hashed_password, salt))
+cur.execute("INSERT INTO users (first_name, last_name, username, email, password, salt) VALUES (%s, %s, %s, %s, %s, %s)", (first_name, last_name, username, email_str, hashed_password, salt))
 conn.commit()
 
 cur.close()
