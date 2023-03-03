f4abc27ac8063ef9b685704bd22a3379ff4fdb3f
Ryan Harper <ryanlharper@gmail.com>
2023-03-03 18:58:38 -0500

    add user specific watchlist

--- watchlist.py
+++ watchlist.py
@@ -1,42 +1,47 @@
 import yfinance as yf
-import config
+import config, login
 import time
 
-def update_watchlist():
-    conn = config.connection
-    cur = conn.cursor()
-    cur.execute("""
-        SELECT ws.symbol
-        FROM watchlist_symbols ws
-        WHERE etf = 'No' 
-        ORDER BY ws.symbol
-    """)
-    symbols = [r[0] for r in cur.fetchall()]
-    
-    while True:
-        try:
-            for symbol in symbols:
-                data = yf.Ticker(symbol).history(period='4d')
-                latest_price = data['Close'][-1]
-                previous_close = data['Close'][-2]
-                change_pct = (latest_price - previous_close) / previous_close * 100
-                
-                cur.execute("""
-                    INSERT INTO watchlist (symbol, price, change_pct)
-                    VALUES (%s, %s, %s)
-                    ON CONFLICT (symbol) DO UPDATE
-                    SET price = excluded.price, change_pct = excluded.change_pct;
-                """, (symbol, latest_price, change_pct))
-                
-                conn.commit()
-                
-                print(f"{symbol}: ${latest_price:.2f} ({change_pct:.2f}%)")
-            
-            time.sleep(60)
-        except KeyboardInterrupt:
-            break
-            
-    cur.close()
-    conn.close()
+user_id = login.login()
 
-update_watchlist()
+# connect to db and query user symbols
+conn = config.connection
+cur = conn.cursor()
+cur.execute("""
+    SELECT ws.symbol
+    FROM watchlist_symbols ws
+    JOIN users_watchlist uw ON 
+    uw.watchlist_symbols_id = ws.id
+    WHERE etf = 'No' AND uw.user_id = %s 
+    ORDER BY ws.symbol
+""", (user_id,))
+symbols = [r[0] for r in cur.fetchall()]
+
+# query yfinance and stream real-time rrices
+while True:
+    try:
+        for symbol in symbols:
+            data = yf.Ticker(symbol).history(period='4d')
+            latest_price = data['Close'][-1]
+            previous_close = data['Close'][-2]
+            change_pct = (latest_price - previous_close) / previous_close * 100
+            
+            cur.execute("""
+                INSERT INTO watchlist (symbol, price, change_pct)
+                VALUES (%s, %s, %s)
+                ON CONFLICT (symbol) DO UPDATE
+                SET price = excluded.price, change_pct = excluded.change_pct;
+            """, (symbol, latest_price, change_pct))
+            
+            conn.commit()
+            
+            print(f"{symbol}: ${latest_price:.2f} ({change_pct:.2f}%)")
+        
+        time.sleep(60)
+    except KeyboardInterrupt:
+        break
+        
+cur.close()
+conn.close()
+
+
