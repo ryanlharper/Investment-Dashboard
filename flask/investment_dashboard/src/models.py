d4e7f9f7c0635619a31adf2e7a513e4b4ac2d3ba
Ryan Harper <ryanlharper@gmail.com>
2023-03-05 19:18:33 -0500

    clean-up

--- flask/investment_dashboard/src/models.py
+++ flask/investment_dashboard/src/models.py
@@ -24,75 +24,6 @@ class User(db.Model):
             'email': self.email,
         }
 
-class UserSelection(db.Model):
-    __tablename__ = 'user_category_subcategory_selections'
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), primary_key=True)
-    subcategory_id = db.Column(db.Integer, db.ForeignKey('subcategories.id'), primary_key=True)
-    selected = db.Column(db.Boolean)
-
-class Category(db.Model):
-    __tablename__ = 'categories'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(128), nullable=False)
-
-    def __init__(self, name: str):
-        self.name = name
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-        }
-        
-class Subcategory(db.Model):
-    __tablename__ = 'subcategories'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(128), nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
-
-    def __init__(self, name: str, category_id: int):
-        self.name = name
-        self.category_id = category_id
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'category_id': self.category_id,
-        }
-
-class BudgetYear(db.Model):
-    __tablename__ = 'budget_years'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    year = db.Column(db.String(4), nullable=False)
-
-class Budget(db.Model):
-    __tablename__ = 'budget_items'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    amount = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
-    year_id = db.Column(db.Integer, db.ForeignKey('budget_years.id'), nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
-    subcategory_id = db.Column(db.Integer, db.ForeignKey('subcategories.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-
-    def __init__(self, amount: float, year_id: int, category_id: int, subcategory_id: int, user_id: int):
-        self.amount = amount
-        self.year_id = year_id
-        self.category_id = category_id
-        self.subcategory_id = subcategory_id
-        self.user_id = user_id
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'amount': str(self.amount),
-            'year_id': self.year_id,
-            'category_id': self.category_id,
-            'subcategory_id': self.subcategory_id,
-            'user_id': self.user_id
-        }
-
 class Transaction(db.Model):
     __tablename__ = 'transactions'  
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -105,8 +36,6 @@ class Transaction(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     number_shares = db.Column(db.Numeric)
     description = db.Column(db.String(128), nullable=True)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
-    subcategory_id = db.Column(db.Integer, db.ForeignKey('subcategories.id'), nullable=False)
     
 
     def __init__(self, amount: float, date: date, category_id: int, subcategory_id: int, user_id: int, description: str):
@@ -122,8 +51,6 @@ class Transaction(db.Model):
             'id': self.id,
             'amount': str(self.amount),
             'date': self.date,
-            'category_id': self.category_id,
-            'subcategory_id': self.subcategory_id,
             'user_id': self.user_id,
             'description': self.description
         }
@@ -137,7 +64,7 @@ class Earnings(db.Model):
 class IndexPrices(db.Model):
     __tablename__ = 'index_prices'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    symbol = db.Column(db.String(128))
+    symbol = db.Column(db.String(128), unique=True)
     level = db.Column(db.Numeric(precision=10, scale=2))
     change_pct = db.Column(db.Numeric(precision=10, scale=2))
 
